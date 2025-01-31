763ab4aa6d4baa35fd8d06ca621274abe167186b
unknown <mubasheer57@gmail.com>
2025-01-31 18:12:54 +0530

    count.terraform

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z0572912MDKFY6PLDT6I"
+}
+
+variable "domain_name" {
+    default = "mlearndevops.online"
+}
